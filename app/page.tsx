06802442f7c1a02dc6c99ebd729045e802cc9be6
muhammednafih09 <muhammednafih09@gmail.com>
2026-02-05 21:26:46 +0530

    Shadcn Installed

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,17 @@ export default function Home() {
       <main className="flex-1">
         {/* Hero Section */}
         <section className="container mx-auto px-4 py-32">
-          <div className="">
-            <h1 className="text-black">
+          <div className="mx-auto max-w-4xl text-center">
+            <h1 className="text-black mb-6 text-6xl font-bold">
               A better way to track your job application.
             </h1>
-            <p className="text-gray-700">
+            <p className="text-muted-foreground mb-10 text-xl">
               Capture, organize, and manage your job search in one place.
             </p>
+            <div className="flex flex-col items-center gap-4">
+              <button>Start For Free</button>
+              <p>Free forever. No credit card required.</p>
+            </div>
           </div>
         </section>
       </main>
