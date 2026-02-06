9e1a2e70230f7af31b8acf15b420066c54906fd4
muhammednafih09 <muhammednafih09@gmail.com>
2026-02-06 23:39:34 +0530

    Dashboard Access Limitted To Logged In Users

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,12 @@
-export default function Dashboard() {
+import { getSession } from "@/lib/auth/auth";
+import { redirect } from "next/navigation";
+
+export default async function Dashboard() {
+  const session = await getSession();
+
+  if (!session?.user) {
+    redirect("/sign-in");
+  }
+
   return <div className="">Dashboard Page</div>;
 }
--- proxy.ts
+++ proxy.ts
@@ -1,13 +1,20 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getSession } from "./lib/auth/auth";
 
-export default async function proxy(resquest: NextRequest) {
+export default async function proxy(request: NextRequest) {
   const session = await getSession();
 
-  const isDashboardPage = resquest.nextUrl.pathname.startsWith("/dashboard");
+  //   const isDashboardPage = request.nextUrl.pathname.startsWith("/dashboard");
 
-  if (isDashboardPage && !session?.user) {
-    return NextResponse.redirect(new URL("/sign-in", resquest.url));
+  //   if (isDashboardPage && !session?.user) {
+  //     return NextResponse.redirect(new URL("/sign-in", request.url));
+  //   }
+
+  const isSignInPage = request.nextUrl.pathname.startsWith("/sign-in");
+  const isSignUpPage = request.nextUrl.pathname.startsWith("/sign-up");
+
+  if ((isSignInPage || isSignUpPage) && session?.user) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
   }
 
   return NextResponse.next();
